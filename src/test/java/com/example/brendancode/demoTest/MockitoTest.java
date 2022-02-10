dc40ab4167b069c8168cadcd75fae3d9ee1a4934
Brendan <ugobrendan@gmail.com>
2022-02-10 10:50:42 +0000

    created jUnit test for coverage

--- src/test/java/com/example/brendancode/demoTest/MockitoTest.java
+++ src/test/java/com/example/brendancode/demoTest/MockitoTest.java
@@ -1,11 +1,14 @@
 package com.example.brendancode.demoTest;
 import com.example.brendancode.demo.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class MockitoTest {
@@ -28,8 +31,12 @@ public class MockitoTest {
     @Test
     public void testAddLanguage(){
         Language saveLanguage = new Language("Test Language");
-        String expected = "saved";
+        String expected = "save";
         String actual = sakilaDbApplication.addLanguage(saveLanguage.getName());
         ArgumentCaptor<Language>languageArgumentCaptor = ArgumentCaptor.forClass(Language.class);
+        verify(languageRepository).save(languageArgumentCaptor.capture());
+        languageArgumentCaptor.getValue();
+        Assertions.assertEquals(expected, actual, "Data has been created in mock DB");
     }
+
 }
--- src/test/java/com/example/brendancode/demoTest/SakilaDbApplicationTests.java
+++ src/test/java/com/example/brendancode/demoTest/SakilaDbApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.brendancode.demoTest;
 
+import com.example.brendancode.demo.Film;
+import com.example.brendancode.demo.Language;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SakilaDbApplicationTests {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-//	@Test
-//	void contextLoads() {
-//	}
+class SakilaDbApplicationTests {
+    private Language language = new Language();
+    private Film film = new Film();
+
+    @Test
+    public void test_getLanguage(){
+        language.setName("testname");
+        assertEquals("testname", language.getName());
+    }
+
+    @Test
+    public void test_getFilm(){
+        film.setTitle("testfilm");
+        assertEquals("testfilm", film.getTitle());
+    }
 
 }
