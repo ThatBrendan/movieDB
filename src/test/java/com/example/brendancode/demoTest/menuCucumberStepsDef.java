0e922d5385b92a5946925bc5a1a61a8d7e03a367
Brendan <ugobrendan@gmail.com>
2022-02-10 10:58:53 +0000

    Trying to get more coverage

--- src/test/java/com/example/brendancode/demoTest/menuCucumberStepsDef.java
+++ src/test/java/com/example/brendancode/demoTest/menuCucumberStepsDef.java
@@ -5,19 +5,19 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class menuCucumberStepsDef {
-    @Given("I have selected a film")
-    public void i_have_selected_a_film() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @When("Film is not in {string}")
-    public void film_is_not_in(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("I would like to see all available languages")
-    public void i_would_like_to_see_all_available_languages() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+//    @Given("I have selected a film")
+//    public void i_have_selected_a_film() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @When("Film is not in {string}")
+//    public void film_is_not_in(String string) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @Then("I would like to see all available languages")
+//    public void i_would_like_to_see_all_available_languages() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
 }
