746347b39a708b019f4b1caf24b7c2f58a5422eb
Brendan <ugobrendan@gmail.com>
2022-02-18 14:58:40 +0000

    Add more code to category test

--- src/test/java/com/example/brendancode/demoTest/CategoryTest.java
+++ src/test/java/com/example/brendancode/demoTest/CategoryTest.java
@@ -4,6 +4,9 @@ import com.example.brendancode.demo.Category;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CategoryTest {
     private Category category = new Category();
 
@@ -11,5 +14,16 @@ public class CategoryTest {
     public void testGetCategory(){
         category.setName("Games");
         Assert.assertEquals("Should display Games category", category.getName(),"Games");
+    }
+
+    @Test
+    public void testCategoryBy_id(){
+        assertEquals(0, category.getCategory_id(), "This test has failed");
+    }
+
+    @Test
+    public void test_constructor(){
+        Category category = new Category();
+        assertTrue(category instanceof Category, "Not the right instance of Category");
     }
 }
