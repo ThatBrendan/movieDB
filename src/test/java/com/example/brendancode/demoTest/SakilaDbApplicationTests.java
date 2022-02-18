c9de4e93860d44c5f2042f183afa1b9f6ea855c7
Brendan <ugobrendan@gmail.com>
2022-02-18 15:47:14 +0000

    Refactored test for main dB

--- src/test/java/com/example/brendancode/demoTest/SakilaDbApplicationTests.java
+++ src/test/java/com/example/brendancode/demoTest/SakilaDbApplicationTests.java
@@ -1,4 +1,8 @@
 package com.example.brendancode.demoTest;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class SakilaDbApplicationTests {
 }
